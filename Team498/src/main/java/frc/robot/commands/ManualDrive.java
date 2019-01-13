19043966c318239482f6c5814a18e392182287be
Jack Haehl <piginatuxedo4@gmail.com>
2019-01-12 18:50:28 -0700

    Finished Classes
    Implemented rampdrive into ManualDrive and finished adding the code from last year that is still relevant.

--- Team498/src/main/java/frc/robot/commands/ManualDrive.java
+++ Team498/src/main/java/frc/robot/commands/ManualDrive.java
@@ -8,11 +8,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.ConstantAccelerationCalculator;
 
 public class ManualDrive extends Command {
+
+  private Operator operator = Operator.getOperator();
+  private Drivetrain drivetrain;
+  private ConstantAccelerationCalculator moveAcceleration = new ConstantAccelerationCalculator(0.00005);
+  private ConstantAccelerationCalculator turnAcceleration = new ConstantAccelerationCalculator(0.00005);
+
+
   public ManualDrive() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    super("RampDrive");
+
+    requires(this.drivetrain = Drivetrain.getDrivetrain());
+
+
   }
 
   // Called just before this Command runs the first time
@@ -23,6 +36,11 @@ public class ManualDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue() - operator.controller.axisRightTrigger.getAxisValue());
+    double turn = moveAcceleration.getNextDataPoint(operator.controller.axisLeftX.getAxisValue());
+
+    this.drivetrain.drive(move, turn);
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -34,11 +52,13 @@ public class ManualDrive extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    drivetrain.drive(0,0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
