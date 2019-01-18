337d12722df905f8a7ab868684314427797f1f3e
Jack Haehl <piginatuxedo4@gmail.com>
2019-01-18 15:58:57 -0700

    Importing the ManualDrive Command into Drivetrain

--- Team498/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Team498/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Mappings;
-
+import frc.robot.commands.ManualDrive;
 
 
 
