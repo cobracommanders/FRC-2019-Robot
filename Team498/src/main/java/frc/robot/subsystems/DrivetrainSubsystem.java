138a72d3ef6705ae65ad31b7067aa234559776ee
Katy Hochstetler <katy@hochstetlers.com>
2019-02-05 18:45:21 -0700

    making some progress with encoders

--- Team498/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ Team498/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -14,11 +14,16 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Mappings;
 import frc.robot.commands.ManualDriveCommand;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
 
 
 
 public class DrivetrainSubsystem extends Subsystem {
-
+  private static final double WheelDiameter = 0.1016; // 4 inch wheels. This was converted to meters
+	private static final double PulsePerRevolution = 1024; // pulses per revolution
+	private static final double WheelCircumference = WheelDiameter * Math.PI;
+  private static final double MetersPerPulse = WheelCircumference / PulsePerRevolution;
+  
   // TODO:will need to change the motor channels.
   private WPI_TalonSRX frontLeftDrive = new WPI_TalonSRX(Mappings.frontLeftDriveMotorChannel);
   private WPI_TalonSRX frontRightDrive = new WPI_TalonSRX(Mappings.frontRightDriveMotorChannel);
@@ -30,6 +35,16 @@ public class DrivetrainSubsystem extends Subsystem {
 
   private DifferentialDrive drive = new DifferentialDrive(leftGroup, rightGroup);
 
+  private SensorCollection leftEncoder = new SensorCollection(frontLeftDrive);
+  private SensorCollection rightEncoder = new SensorCollection(frontRightDrive);
+
+  
+
+
+  public DrivetrainSubsystem() {
+    super("DrivetrainSubsystem");
+  }
+
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new ManualDriveCommand());
@@ -37,6 +52,10 @@ public class DrivetrainSubsystem extends Subsystem {
 
   public void drive(double move, double turn) {
     drive.arcadeDrive(move, turn);
+  }
+
+  public double getPulseWidthPosition() {
+    return ((leftEncoder.getPulseWidthPosition() + rightEncoder.getPulseWidthPosition()) / 2);
   }
 
 }
