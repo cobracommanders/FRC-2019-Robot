0ea1872344c25cf680f92a6579f4093f04644bc3
Katy Hochstetler <katy@hochstetlers.com>
2019-02-16 12:52:28 -0700

    fixed merge

--- Team498/src/main/java/frc/robot/commands/ReleaseRampCommand.java
+++ Team498/src/main/java/frc/robot/commands/ReleaseRampCommand.java
@@ -21,7 +21,7 @@ public class ReleaseRampCommand extends InstantCommand {
   protected void initialize() {
     Robot.ramp.releaseRamp();
         try {
-            this.wait((long) 10000);
+            this.wait((long) 5000);
         } catch (Exception e) {
             // Swallow the exception
         } 
